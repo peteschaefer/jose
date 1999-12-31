9d023d59b164f37d6422dd337d9a5260967865da
Peter Schaefer <pete.schaefer@gmail.com>
1999-12-31 23:59:59 +0000

    fixed: arrow search order

--- src/de/jose/view/BoardPanel.java
+++ src/de/jose/view/BoardPanel.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of the Jose Project
  * see http://jose-chess.sourceforge.net/
- * (c) 2002-2006 Peter Schäfer
+ * (c) 2002-2006 Peter Schï¿½fer
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -659,7 +659,7 @@ public class BoardPanel
 				return (a.from - b.from);
 			int la = EngUtil.euclidDistSq(a.from,a.to);
 			int lb = EngUtil.euclidDistSq(b.from,b.to);
-			return la-lb;
+			return lb-la;
 		}
 	}
 
