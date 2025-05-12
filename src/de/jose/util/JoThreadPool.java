d8b913a37d65aac43df5d6daaa0b9eacecfec69d
Peter Schäfer <pete.schaefer@gmail.com>
2025-05-12 09:51:34 +0200

    * parallel experiments on a 3M database -> surprisingly, sequential is better !

--- src/de/jose/util/JoThreadPool.java
+++ src/de/jose/util/JoThreadPool.java
@@ -6,6 +6,8 @@ import java.util.concurrent.*;
 
 /**
  * A thread pool with more flexible shutdown phase.
+ * finish() and abort() wait for tasks to finish, without having to enter the shutdown() phase.
+ *
  */
 public class JoThreadPool<R extends Runnable> extends ThreadPoolExecutor
 {
@@ -38,11 +40,20 @@ public class JoThreadPool<R extends Runnable> extends ThreadPoolExecutor
         return queueWatermark;
     }
 
+    /**
+     * drop waiting tasks, waiting for executing tasks to finish
+     */
     public void abort() {
         getQueue().clear();
         finish();
     }
 
+    /**
+     * wait for all tasks to finish
+     * (note: this is similar to ThreadPoolExecutor.awaitTermination() but without having to enter the shutdown() phase.
+     * After all tasks have finished, new tasks may be submitted, again (which is not possible after ThreadPoolExecutor.shutdown());
+     *
+     */
     public void finish() {
         while(getActiveCount() > 0) {
             closingThread = Thread.currentThread();
