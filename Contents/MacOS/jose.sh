39e93608984eb0226de3da18b191f11b6cdeda67
hrimfaxi <pete.schaefer@gmail.com>
2025-06-18 05:49:18 +0200

    test case for MoreGameCache 16M rows need abt 3GB of memory speedup is great

--- Contents/MacOS/jose.sh
+++ Contents/MacOS/jose.sh
@@ -2,7 +2,7 @@
 
 cd $(dirname "$0")/../..
 
-vmargs=-Djava.library.path=lib/Mac_intel
+vmargs=-Djava.library.path=lib/Mac_intel -Xmx64G
 args=jose.db=MySQL-standalone
 
 jre/bin/java $vmargs -jar jose.jar $args
