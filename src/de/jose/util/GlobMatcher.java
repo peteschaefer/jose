f0683ebae4d9f375f827b303448d1adaed0c0023
hrimfaxi <pete.schaefer@gmail.com>
2025-07-20 12:08:40 +0200

    work in progress (todo regex query prefix)

--- src/de/jose/util/GlobMatcher.java
+++ src/de/jose/util/GlobMatcher.java
@@ -14,15 +14,22 @@ public class GlobMatcher
 
     private boolean caseSensitive=false;
     private boolean accentSensitive=false;
+    private boolean greedy=false;
     private String pattern;
 
-    private static int match_max(int m1, int m2) {
+    private static int mmax(int m1, int m2) {
         if (m1 < 0) return m2;
+        if (m2 < 0) return m1;
         return Math.max(m1, m2);
     }
-    private int plus_one(int m1) {
+    private static int mmin(int m1, int m2) {
+        if (m1 < 0) return m2;
+        if (m2 < 0) return m1;
+        return Math.min(m1, m2);
+    }
+    private static int mplus(int m1,int i) {
         if (m1 < 0) return m1;
-        return m1 + 1;
+        return m1 + i;
     }
 
     private int match(CharSequence p0, CharSequence i0) {
@@ -35,8 +42,12 @@ public class GlobMatcher
         CharSequence p1 = p0.subSequence(1, p0.length());
         CharSequence i1 = i0.subSequence(1, i0.length());
         switch (cp) {
-            case '*':   return match_max( plus_one(match(p0,i1)), match(p1,i0) );
-            case '?':   return plus_one(match(p1, i1));
+            case '*':
+                int m1 = mplus(match(p0,i1),1);
+                int m2 = match(p1,i0);
+                return greedy? mmax(m1,m2) : mmin(m1,m2);
+            case '?':
+                return mplus(match(p1, i1),1);
         }
         //  else match 1 character
         char ci = i0.charAt(0);
@@ -49,9 +60,9 @@ public class GlobMatcher
             ci = CharUtil.stripDiacritic(ci);
         }
         if (cp != ci)
-            return 0;
+            return -i0.length();
         else
-            return plus_one(match(p1,i1));
-    //  todo unroll tail-recursion
+            return mplus(match(p1,i1),1);
+    //  todo unroll tail-recursion?
     }
 }
