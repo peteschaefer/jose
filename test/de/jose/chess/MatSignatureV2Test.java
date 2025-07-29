e3466f3ebab1aabfdb403ec77df63e69fb1cf0bd
hrimfaxi <pete.schaefer@gmail.com>
2025-07-29 21:47:18 +0200

    more experiments

--- test/de/jose/chess/MatSignatureV2Test.java
+++ test/de/jose/chess/MatSignatureV2Test.java
@@ -1173,14 +1173,16 @@ class MatSignatureV2Test {
     @Test
     void testBinMatchPooled() throws Exception {
         String fen = "2r5/p1p2bpr/3pkp2/2p3p1/P1P1P1P1/1P1RNPKP/7R/8 b - - 36 1";
-        String sql1 = "select task_pop(0) as Result " +
+        String sql1 = "select task_pop(0) as Result, 1 as Phase " +
                         " from MoreGame" +
-                        " where task_push(GId, WhiteSignature,BlackSignature, Fen,Bin,LOCATE(0xf0,Bin), ?,?) "+
-                        "   and task_peek(0) "+
-                " union all"+
-                        " select task_pop(1) as Result" +  //  collects outstanding results; blocks if necessary
+                        " where sig_match(WhiteSignature,BlackSignature,LOCATE(0xf0,Bin), ?,?)" +
+                        "   and task_push(GId,0,0, Fen,Bin,LOCATE(0xf0,Bin), ?,?) "+
+                        "   and task_peek(0) "+     //  collects result if available
+                " union all ("+
+                        " select task_pop(1) as Result, 2 as Phase" +  //  collects outstanding results; blocks if necessary
                         " from MoreGame"+
-                        " where task_peek(1) ";
+                        " where task_peek(1) " +
+                        " limit 254000)";   //  max. number of buckets = table size / bucket_size
 /*     crucial that:
         - task_push_pop() is only called for rows that have passed sig_match()
             returns a bucket of (unrelated!!,possibly empty) results
@@ -1188,11 +1190,19 @@ class MatSignatureV2Test {
             'from MoreGame' creates a loop of sufficient length
             break as soon as NULL is encountered
  */
-/*   'having' is used to skip null results. But the evaluation order is crucial:
+/*   task_peek() is used to skip null results. But the evaluation order is crucial:
         in phase 2:     if task_peek()==1 then task_pop()   OK
         in phase 1:     task_push(), if task_peek() then task_pop()
                 i.e., it is crucial that task_push() must be called before task_peek()
-                note that task_push() >= 0 is always true. We want task_push() be called (before having).
+                note that task_push() >= 0 is always true.
+    *//*
+        first section of UNION needs abt. 9 secs (early-cut-off check over all rows)
+        second section needs 20 secs (=waiting for BinReader tasks to complete).
+        jose, josemi does it < 10 secs.
+        Why is this so slow???
+        todo increase input bucket size?
+
+        sig_match() and task_push() can be combined into one call. No noticeable difference, however.
     */
         withDBServer();
 
@@ -1205,8 +1215,8 @@ class MatSignatureV2Test {
         JoPreparedStatement pstm = new JoPreparedStatement(conn,sql1,ResultSet.TYPE_FORWARD_ONLY,ResultSet.CONCUR_READ_ONLY);
         pstm.setString(1, fen);
         pstm.setInt(2, POS_EXACT);
-//        pstm.setString(3, fen);
-//        pstm.setInt(4, POS_EXACT);
+        pstm.setString(3, fen);
+        pstm.setInt(4, POS_EXACT);
 //        pstm.setString(5, fen);
 //        pstm.setInt(6, POS_EXACT);
         pstm.setFetchSize(Integer.MIN_VALUE);
@@ -1215,9 +1225,9 @@ class MatSignatureV2Test {
         ResultSet res = pstm.getResultSet();
         while(res.next()) {
             rowCount++;
-            //int phase = res.getInt(1);
             byte[] bucket = res.getBytes(1);
-            //if (phase==2 && res.wasNull()) break;
+            int phase = res.getInt(2);
+            if (phase==2 && bucket!=null) break;
 
             foundRowCount += processResults(bucket);
         }
