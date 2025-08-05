dc34d919c31050cee7163d1e887b20d0dea67bdc
Peter Schäfer <pete.schaefer@gmail.com>
2025-08-05 14:17:03 +0200

    some safe-guards with ThreadPool::finish

--- src/de/jose/util/concurrent/QueueThreadPool.java
+++ src/de/jose/util/concurrent/QueueThreadPool.java
@@ -1,6 +1,6 @@
 package de.jose.util.concurrent;
 
-import java.util.Iterator;
+import java.util.*;
 import java.util.concurrent.*;
 
 /**
@@ -80,9 +80,11 @@ public class QueueThreadPool<R extends Runnable> extends ThreadPoolExecutor
     public void finish() {
         //while(getActiveCount() > 0 && getQueue().size() > 0) {
         //  note: getActiveCount() is unreliable. May return too early, leaving jobs not done.
+        ArrayList<Throwable> errors = new ArrayList();
         while(!futures.isEmpty()) {
             closingThread = Thread.currentThread();
             try {
+                //  poll pending Futures
                 Iterator<Future<R>> i =  futures.keySet().iterator();
                 while(i.hasNext()) {
                     Future<R> f = i.next();
@@ -92,16 +94,33 @@ public class QueueThreadPool<R extends Runnable> extends ThreadPoolExecutor
                     i.remove();
                 }
             } catch (InterruptedException e) {
+                //  when a job finished and woke up closingThread
                 continue;
             } catch (ExecutionException e) {
-                //  job threw
-                throw new RuntimeException(e.getCause());
+                //  when there was an exception in the job itself
+                errors.add(e.getCause());
+                continue;
             } catch (TimeoutException e) {
+                //  when the timeout on get() elapsed
                 System.err.println("[bored of waiting]");
+                continue;
+            } catch(NoSuchElementException nse) {
+                //  when the map got empty between hasNext() and next()
+                continue;
+            } catch(ConcurrentModificationException cme) {
+                //  when the map was edited while iterating
+                //  no matter. keep polling
                 continue;
             }
         }
         closingThread = null;
+
+        if (errors.size() > 0) {
+            RuntimeException rte = new RuntimeException(errors.get(0));
+            for(Throwable t : errors)
+                rte.addSuppressed(t);
+            throw rte;
+        }
     }
 
     @Override
